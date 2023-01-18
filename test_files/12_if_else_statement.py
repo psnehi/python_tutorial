9fcabee0d3f6a1909235ea648b90b150e859af6c
PRASHANT KUMAR SNEHI <prashant@tsiplc.com>
2023-01-19 00:26:26 +0530

    if_elif_else

---
+++ test_files/12_if_else_statement.py
@@ -0,0 +1,19 @@
+num = int(input("Enter any number: "))
+if num == 0:
+    print('It is zero')
+elif num % 2 == 0:
+    print('It is even number')
+else:
+    print('It is odd number')
+
+number = int(input("Enter the number: "))
+if number == 10:
+    print("number is equals to 10")
+elif number == 50:
+    print("number is equal to 50")
+elif number == 100:
+    print("number is equal to 100")
+else:
+    print("number is not equal to 10, 50 or 100")
+
+
