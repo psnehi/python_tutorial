7ec96710a597ce3e8475d60ae968e0113240195f
PRASHANT KUMAR SNEHI <prashant@tsiplc.com>
2023-01-19 00:21:16 +0530

    strings

---
+++ test_files/9_strings.py
@@ -0,0 +1,13 @@
+# single line
+string = "Prashant"
+print(f'Single line string: {string}')
+
+multiline_string = "Prashant\
+ Snehi"
+print(f'Multiline string using backslash: {multiline_string}')
+
+multiline_string = '''My name is 
+Prashant Kumar Snehi
+'''
+
+print(f'Multiline string using triple quotation marks: {multiline_string}')
