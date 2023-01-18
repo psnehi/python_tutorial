8dbc04a98c637f901e58aba2b4e117c7a35cbb27
PRASHANT KUMAR SNEHI <prashant@tsiplc.com>
2023-01-19 00:48:26 +0530

    python nested loops

---
+++ test_files/14_nested_loops.py
@@ -0,0 +1,11 @@
+import random
+
+numbers = []
+
+for al in range(0, 11):
+    numbers.append(random.randint(0, 11))
+
+for num in range(0, 11):
+    for i in numbers:
+        if num == i:
+            print(num, end=" ")
