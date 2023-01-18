eaafe8f7f206d45657762f6240d9700cd5f6ea40
PRASHANT KUMAR SNEHI <prashant@tsiplc.com>
2023-01-19 00:20:13 +0530

    keywords

---
+++ test_files/3_keywords.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate the application of iskeyword()
+# importing keyword library which has lists
+import keyword
+
+# displaying the complete list using "kwlist()."
+print(f'The set of keyword in this version is: ')
+print(keyword.kwlist)
+
+# using help() you can retrieved the currently offered keywords
+help("keywords")
+
+print( 4 == 4)
+print(True or False)
+print(True and False)
